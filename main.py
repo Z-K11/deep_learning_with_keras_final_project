7960bb239e0af86516b6d9000dd88f7695495b47
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-16 12:35:51 +0500

    split data into training and test sets and train the model using 10 neurons and 1 hidden layer and the training 50 different models for 50 different evalution scores

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Input
+from tensorflow.keras.layers import Input
 from keras.layers import Dense
 data=pd.read_csv('csv_data/concrete_data.csv')
 print(data.head())
@@ -11,5 +11,22 @@ data_column = data.columns
 predictors = data[data_column[data_column!='Strength']]
 target=data['Strength']
 from sklearn.model_selection import train_test_split
-xtrain,ytrain,xtest,ytest=train_test_split(predictors,target,test_size=30,random_state=4)
 print(predictors.shape)
+input_params = predictors.shape[1]
+def regression_model():
+    model = Sequential()
+    model.add(Input(shape=(input_params,)))
+    model.add(Dense(10,activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='adam',loss='mse')
+    return model
+mse=[]
+for i in range(50):
+    print('Iteration {}'.format(i+1))
+    xtrain,xtest,ytrain,ytest=train_test_split(predictors,target,test_size=0.3,random_state=i+1)
+    model=regression_model()
+    model.fit(xtrain,ytrain,epochs=50,verbose=1)
+    score=model.evaluate(xtest,ytest,verbose=1)
+    mse.append(score)
+print('Mean of the mean squared error {:.4f}'.format(np.mean(mse)))
+print('Standard Deviation of the mean squared error {:.4f}'.format(np.std(mse)))
